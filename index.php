d71d03f5510da1c9282429ec7bf64fd19f5eb9e6
Raúl Alejandro Fuentes Blanco <rulazw@gmail.com>
2018-04-21 23:10:42 -0500

    cambios

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php header( 'Location:index.html' ) ; ?>
-
-
+<?php $pdo = new PDO("sql:host=uso1.c7dhdhum783t.us-east-2.rds.amazonaws.com;dbname=USO_INTELIGENTE","mota","JavierRM1");
+?>
---
+++ prueba.php
@@ -0,0 +1,10 @@
+<?php $pdo = new PDO("sql:host=uso1.c7dhdhum783t.us-east-2.rds.amazonaws.com;dbname=USO_INTELIGENTE","mota","JavierRM1");?>
+
+<html>
+ <head>
+  <title>Prueba de PHP</title>
+ </head>
+ <body>
+ <?php echo '<p>Hola Mundo</p>'; ?>
+ </body>
+</html>
