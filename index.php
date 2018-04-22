f8f54a89696ae54eb3f13269ba9521f1a60968f7
Raúl Alejandro Fuentes Blanco <rulazw@gmail.com>
2018-04-21 22:42:04 -0500

    Cambios index.php

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-<?php header( 'Location:index.html' ) ;  ?>
+<?php header( 'Location:index.html' ) ;
+echo
+$servername = "uso1.c7dhdhum783t.us-east-2.rds.amazonaws.com";
+$username = "mota";
+$password = "JavierRM1";
+$dbname = "USO_INTELIGENTE";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully";
+
+
+ ?>
+
+
